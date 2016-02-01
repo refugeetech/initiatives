4232022a030434eba24496ae7518be26097c1325
Angamanga <ki222bj@student.lnu.se>
2016-02-01 20:22:49 +0100

    adding blog to navbar

--- blog/blog.js
+++ blog/blog.js
@@ -1,11 +1,10 @@
 //Configuration for ryw:blog package
 // check out the README: https://github.com/meteor-blog/meteor-blog
 
-/*
+
 Blog.config({
   basePath: '/blog', // '/myBlog', '/myBlog/my-post', '/myBlog/tag/whatever', etc.
-  adminBasePath: '/admin/blog',
+  //adminBasePath: '/admin/blog',
   blogIndexTemplate: 'allPosts', // '/blog' route
   blogShowTemplate: 'singlePost'    // '/blog/:slug' route
 });
-*/
